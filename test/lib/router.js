c22637718680f84d6f8b1a4dd98c55d1e474ace0
Jaynish <jaynish@improwised.com>
2015-12-15 18:18:52 +0530

    Added failing test for incorrect parameter handling

--- test/lib/router.js
+++ test/lib/router.js
@@ -782,6 +782,34 @@ describe('Router', function() {
             });
         });
     });
+
+    it('resolves non-parameterized routes without attached parameters', function(done) {
+      var app = koa();
+      var router = new Router();
+
+      router.get('/notparameter', function *(next) {
+        this.body = {
+          param: this.params.parameter,
+        };
+      });
+
+      router.get('/:parameter', function *(next) {
+        this.body = {
+          param: this.params.parameter,
+        };
+      });
+
+      app.use(router.routes());
+      request(http.createServer(app.callback()))
+        .get('/notparameter')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+
+          expect(res.body.param).to.equal(undefined);
+          done();
+        });
+    });
   });
 
   describe('Router#use()', function (done) {
